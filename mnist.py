e3b4847a3ff45963ab76a108c13ee1ebe24456fe
Sean Pollock <seanpollock3344@gmail.com>
2017-09-13 19:00:04 -0700

    y prime tensor for estimates

--- mnist.py
+++ mnist.py
@@ -1,16 +1,19 @@
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
-
 import tensorflow as tf
 
+# GRAPH
+
 x = tf.placeholder(tf.float32, [None, 784])
-
 W = tf.Variable(tf.zeros([784, 10]))
-
 b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-print('W', W)
-print('b', b)
+# TRAINING 
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 
 
