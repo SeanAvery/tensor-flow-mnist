a7d985cce4887e473481790180481527d77bb8ee
Sean Pollock <seanpollock3344@gmail.com>
2017-09-14 07:01:25 -0700

    using placeholders

--- practice.py
+++ practice.py
@@ -4,8 +4,11 @@ node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0, dtype=tf.float32)
 node3 = tf.add(node1, node2)
 
-sess = tf.Session()
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b
 
-print(sess.run(node3))
+sess = tf.Session()
+print(sess.run(adder_node, {a: 5, b: 7}))
 
  
