bf414027831df68b62ab775b3d20a945d8392e38
Sean Pollock <seanpollock3344@gmail.com>
2017-09-14 07:21:16 -0700

    getting loss value

--- practice.py
+++ practice.py
@@ -14,9 +14,16 @@ b = tf.Variable([-.3], dtype=tf.float32)
 x = tf.placeholder(tf.float32)
 linear_model = W * x + b
 
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
 print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
  
