7c597db60a80e2cd71bd1caca28fec5bbc4a406a
Sean Pollock <seanpollock3344@gmail.com>
2017-09-13 18:00:44 -0700

    importing tensor flow

--- mnist.py
+++ mnist.py
@@ -1,3 +1,9 @@
 from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
-print(input_data)
+import tensorflow as tf
+
+print('tf', tf)
+
+
+
